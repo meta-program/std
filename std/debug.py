8d4d1aebc6590ec48d7ef4c4850ba5c9e81e25cd
CosmosZhou <cosmos.zhou@airudder.com>
2026-02-15 22:05:44 +0800

    Decorator that set a breakpoint for a remote function within a worker/driver

--- std/debug.py
+++ std/debug.py
@@ -1,5 +1,5 @@
-import functools, traceback
-from std import exec_generator
+import functools, traceback, sys, inspect
+from std import exec_generator, __set__
 
 
 def compare_element(v, _v):
@@ -83,3 +83,76 @@ def default_on_error(default=None):
                 return default
         return wrapper
     return decorator
+
+try:
+    import debugpy
+    def breakpoint(func, port=5678):
+        """
+        Decorator that set a breakpoint for a remote function within a worker/driver
+        """
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                import debugpy
+                print(f"listen to 0.0.0.0:{port}")
+                debugpy.listen(("0.0.0.0", port))
+                print("debugpy.wait_for_client()")
+                debugpy.wait_for_client()
+                print("starting debuging")
+                debugpy.breakpoint()
+                return func(*args, **kwargs)
+            except Exception as e:
+                traceback.print_exc()
+                print(e)
+                raise e
+        if isinstance(func, property):
+            module = sys.modules[func.fget.__module__]
+            new_func = type(func)(lambda self: wrapper(self))
+            cls = getattr(module, func.fget.__qualname__.split(".")[0])
+        elif inspect.isfunction(func) and func.__qualname__ == func.__name__:
+            cls = sys.modules[func.__module__]
+            assert getattr(cls, func.__name__) is func
+            new_func = wrapper
+        else:
+            # class unbound method
+            module = sys.modules[func.__module__]
+            cls = getattr(module, func.__qualname__.split(".")[0])
+            match cls.__dict__[func.__name__]:
+                case staticmethod():
+                    new_func = staticmethod(wrapper)
+                case classmethod():
+                    new_func = classmethod(wrapper)
+                case _:
+                    new_func = wrapper
+        __set__(cls)(new_func)
+
+except ImportError:
+    ...
+
+
+def something_to_debug():
+    print("hello world")
+
+class ExampleClass:
+    def something_to_debug(self):
+        print("hello world")
+
+if __name__ == "__main__":
+    # module function
+    from std import search
+    breakpoint(search.sunday)
+    # unbound class method
+    from std.parser.xml import XMLParser
+    breakpoint(XMLParser.build)
+    # staticmethod
+    from std.sets import Union
+    breakpoint(Union.new)
+    # classmethod
+    from std.parser.markdown import MarkdownI
+    breakpoint(MarkdownI.try_pattern)
+    # property
+    from std.file import Text
+    breakpoint(Text.size)
+
+
+
